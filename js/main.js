eeb2d87ea61556722a3b82d6d681ef52d50a7a80
Essam2715 <essam2715@gmail.com>
2019-07-25 16:18:28 +0200

    Final Edit

--- js/main.js
+++ js/main.js
@@ -1,77 +1,76 @@
+// Animations initialization
+new WOW().init();
 
-
-if (window.matchMedia( "(max-width: 768px)" ).matches) {
-
-  var swiper = new Swiper('.swiperatneen', {
+if (window.matchMedia("(max-width: 768px)").matches) {
+  var swiper = new Swiper(".swiperatneen", {
     slidesPerView: 1,
     spaceBetween: 0,
     freeMode: true,
     // loop:true,
     pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+      el: ".swiper-pagination",
+      clickable: true
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+} else if (window.matchMedia("(max-width: 1000px)").matches) {
+  var swiper = new Swiper(".swiperatneen", {
+    slidesPerView: 2,
+    spaceBetween: 0,
+    freeMode: true,
+    // loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
     },
-  });} else if (window.matchMedia( "(max-width: 1000px)" ).matches){
-    var swiper = new Swiper('.swiperatneen', {
-      slidesPerView: 2,
-      spaceBetween: 0,
-      freeMode: true,
-      // loop:true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    })
-  }
-else {
-    
-var swiper = new Swiper('.swiperatneen', {
-  slidesPerView: 4,
-  spaceBetween: 0,
-  freeMode: true,
-  // loop:true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+} else {
+  var swiper = new Swiper(".swiperatneen", {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    freeMode: true,
+    // loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
 }
 
-    var swiper = new Swiper('.packges .swiper-container', {
-      slidesPerView: 3,
-      slidesPerColumn: 2,
-      spaceBetween: 30,
-      // loop:true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
+var swiper = new Swiper(".packges .swiper-container", {
+  slidesPerView: 3,
+  slidesPerColumn: 2,
+  spaceBetween: 30,
+  // loop:true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
 
-    var swiper = new Swiper('.packges2 .swiper-container', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
+var swiper = new Swiper(".packges2 .swiper-container", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
 
-    $(window).on('load', function () {
-      $('body').css('overflow', 'auto')
-      $('.loader-page').fadeOut(1000)
-  })
+$(window).on("load", function() {
+  $("body").css("overflow", "auto");
+  $(".loader-page").fadeOut(1000);
+});
